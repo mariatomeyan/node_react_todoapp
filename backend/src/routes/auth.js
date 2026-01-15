8ca21447813c7bce2ce315fb79d950de0ddd5009
mariatomeyan <maria.tomeyan70@gmail.com>
2026-01-15 12:01:18 +0100

    fix - wrong email variable to be inserted in register query

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -64,7 +64,7 @@ router.post('/register', async (req, res)=> {
 
         const passwordHash = await bcrypt.hash(password, 10);
         const result = await db.query(
-            'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email', [email, passwordHash]
+            'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email', [normalizedEmail, passwordHash]
         );
 
         const user = result.rows[0];
